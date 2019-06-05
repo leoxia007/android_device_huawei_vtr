01e96406eb10f2196fa460e76f6e06a307a6f769
leoxia007 <xialei198785@gmail.com>
2019-06-05 14:05:26 +0800

    Prevent aptouch_daemon from running

--- device.mk
+++ device.mk
@@ -32,6 +32,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
+# Device init scripts
+PRODUCT_PACKAGES += \
+    init.aptouch.rc
+
 #Display
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.hw=1 \
